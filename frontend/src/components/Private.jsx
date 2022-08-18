0533707332190df318cfff14add76162a0b0c556
thongdang2708 <duythong2708@gmail.com>
2022-08-18 23:40:31 +0300

    prepare for deploy

--- frontend/src/components/Private.jsx
+++ frontend/src/components/Private.jsx
@@ -1,14 +1,30 @@
 
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { authStatus } from '../hooks/authStatus';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Spinner from './Spinner';
 
 
 //Set private for routes with user only, else requires to log in to get priviledge
 const Private = () => {
 
-    let {loggedIn, checkStatus} = authStatus();
+    let [loggedIn, setLoggedIn] = useState(false);
+    let [checkStatus, setCheckStatus] = useState(true);
+    let {user} = useSelector(state => state.auth);
+
+    useEffect(() => {
+
+        if (user) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        };
+
+        setCheckStatus(false);
+        
+    },[user]);
 
     if (checkStatus) {
         return <Spinner />
